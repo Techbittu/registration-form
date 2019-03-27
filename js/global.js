ca011a39976d29b65c393790c30724ba26cea2e7
Deepak Kumar Arya <30655714+Techbittu@users.noreply.github.com>
2019-03-28 00:49:43 +0530

    Update global.js

--- js/global.js
+++ js/global.js
@@ -68,3 +68,17 @@
     
 
 })(jQuery);
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("acc");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
